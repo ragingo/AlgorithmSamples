a1fe3902d5e543c67ffe9d09627c4f4e6f3109e5
ragingo <4784032+ragingo@users.noreply.github.com>
2025-08-15 20:59:39 +0900

    [cpp] バブルソートのデモ用コードをリファクタ

--- cpp/demos/sort/bubble_sort.cpp
+++ cpp/demos/sort/bubble_sort.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <numeric>
 #include <random>
+#include <string>
 #include <vector>
 
 static void bubble_demo([[maybe_unused]] const std::vector<std::string>& args) {
@@ -12,7 +13,7 @@ static void bubble_demo([[maybe_unused]] const std::vector<std::string>& args) {
     std::iota(v.begin(), v.end(), 1);
     std::shuffle(v.begin(), v.end(), std::mt19937(std::random_device()()));
 
-    bubble_sort(v.data(), v.data() + v.size(), std::less<decltype(v)::value_type>());
+    bubble_sort(v.begin(), v.end(), std::less<>());
     for (auto n : v) {
         std::cout << n << ' ';
     }
