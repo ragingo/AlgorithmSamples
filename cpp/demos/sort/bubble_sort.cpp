bbe6927e86fbe771e49314e5289166c8ab44e525
ragingo <4784032+ragingo@users.noreply.github.com>
2025-08-17 15:34:38 +0900

    [cpp] [bubble_sort] ランダムアクセスイテレータでない場合 std::distance は距離の計算にループを回していて非効率なので修正
    ソートが発生しない要素数0と1を直接チェックする

--- cpp/demos/sort/bubble_sort.cpp
+++ cpp/demos/sort/bubble_sort.cpp
@@ -9,16 +9,29 @@
 #include <string>
 #include <vector>
 
-static void bubble_demo([[maybe_unused]] const std::vector<std::string>& args) {
-    std::vector<int> v(10'000);
+constexpr auto ELEMENT_COUNT = 10'000;
+
+static void bubble_sort_demo([[maybe_unused]] const std::vector<std::string>& args) {
+    std::cout << "Bubble Sort Demo" << std::endl;
+    std::cout << ELEMENT_COUNT << " 件のデータを準備します..." << std::endl;
+
+    std::vector<int> v(ELEMENT_COUNT);
     std::iota(v.begin(), v.end(), 1);
+
+    std::cout << ELEMENT_COUNT << " 件のデータをシャッフルします..." << std::endl;
+
     std::shuffle(v.begin(), v.end(), std::mt19937(std::random_device()()));
 
+    std::cout << ELEMENT_COUNT << " 件のデータをソートします..." << std::endl;
+
     bubble_sort(v.begin(), v.end(), std::less<>());
+
+    std::cout << ELEMENT_COUNT << " 件のデータのソートが完了しました。" << std::endl;
+
     for (auto n : v) {
         std::cout << n << ' ';
     }
     std::cout << '\n';
 }
 
-REGISTER_DEMO(bubble, bubble_demo);
+REGISTER_DEMO(bubble_sort, bubble_sort_demo);
