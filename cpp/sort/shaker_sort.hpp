276b794760b141f754c0b1990e269671ac8fb077
ragingo <4784032+ragingo@users.noreply.github.com>
2025-08-30 17:41:06 +0900

    [cpp] [shaker_sort] シェーカーソートを実装

---
+++ cpp/sort/shaker_sort.hpp
@@ -0,0 +1,83 @@
+﻿#pragma once
+#include <array>
+#include <cassert>
+#include <concepts>
+#include <iterator>
+#include <type_traits>
+#include <utility>
+
+namespace AlgorithmSamples::Sort {
+
+template <std::random_access_iterator Iterator, typename Comparator = std::less<>, std::integral Result = size_t>
+constexpr Result shaker_sort(Iterator begin, Iterator end, Comparator comparator = {}) {
+    if (begin == end || std::next(begin) == end) {
+        return 0;
+    }
+
+    Result loopCount = 0;
+
+    auto left = begin;
+    auto right = std::prev(end);
+
+    while (left < right) {
+        // left to right
+        for (auto i = left; i != right; ++i) {
+            auto next = std::next(i);
+            if (comparator(*next, *i)) {
+                std::swap(*i, *next);
+            }
+            ++loopCount;
+        }
+        --right;
+
+        // right to left
+        for (auto i = right; i != left; --i) {
+            auto prev = std::prev(i);
+            if (comparator(*i, *prev)) {
+                std::swap(*i, *prev);
+            }
+            ++loopCount;
+        }
+        ++left;
+    }
+
+    return loopCount;
+}
+
+template <std::integral T, std::size_t N, typename Comparator = std::less<>>
+constexpr std::array<T, N> shaker_sort_const(const std::array<T, N>& input, Comparator comparator = {}) {
+    std::array<T, N> arr = input;
+    if (input.begin() == input.end() || std::next(input.begin()) == input.end()) {
+        return arr;
+    }
+
+    auto left = arr.begin();
+    auto right = std::prev(arr.end());
+
+    while (left < right) {
+        // left to right
+        for (auto i = left; i != right; ++i) {
+            auto next = std::next(i);
+            if (comparator(*next, *i)) {
+                std::swap(*i, *next);
+            }
+        }
+        --right;
+
+        // right to left
+        for (auto i = right; i != left; --i) {
+            auto prev = std::prev(i);
+            if (comparator(*i, *prev)) {
+                std::swap(*i, *prev);
+            }
+        }
+        ++left;
+    }
+
+    return arr;
+}
+
+static_assert(shaker_sort_const(std::array{5, 3, 1, 4, 2}) == std::array{1, 2, 3, 4, 5});
+static_assert(shaker_sort_const(std::array{5, 3, 1, 4, 2}, std::greater<>()) == std::array{5, 4, 3, 2, 1});
+
+}  // namespace AlgorithmSamples::Sort
